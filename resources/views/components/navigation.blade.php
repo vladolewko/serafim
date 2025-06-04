d11c9d2dfc5efd62c3c63a837a35d1c6ae57a9f5
Evg33n <evgen.gulevaty@gmail.com>
2025-06-04 17:35:14 +0300

    fix header and make 2 section

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,7 +1,7 @@
-<nav class="relative z-10 mb-5 w-4/6 mx-auto">
+<nav class="relative z-10 mb-5 w-4/5 mx-auto">
 
-    <ul class="flex justify-around text-white">
-        <li class="text-yellow-200 underline decoration-yellow-200">що таке комплект знань</li>
+    <ul class="flex justify-around text-white text-lg">
+        <li class="text-yellow-400 underline underline-offset-4 decoration-yellow-400">що таке комплект знань</li>
         <li>обрати комплект</li>
         <li>про автора</li>
         <li>соцмережі</li>
