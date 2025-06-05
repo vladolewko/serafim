12f013ff0d44d491f4cdf19fa1b58bccb3f9bfba
Evg33n <evgen.gulevaty@gmail.com>
2025-06-05 20:08:13 +0300

    make footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,57 @@
 </div>
 </main>
-<footer>
+<footer class="w-full bg-blue-400 py-10">
+    <div class="flex w-4/6 justify-between items-start mx-auto ">
+        <div class="flex items-center self-center w-2/6 justify-center text-white gap-2">
+            <img class="h-[140px] w-[140px]" src="{{ asset('img/logo.svg') }}" alt="">
+            <img src="{{ asset('img/line_vertical.svg') }}" alt="">
+            <div class="flex flex-col gap-1">
+                <p class="text-2xl">серафим</p>
+                <p class="text-sm">від солдата - для людей</p>
+                <div class="flex gap-2">
+                    <img src="{{ asset('img/youtube_footer.svg') }}" alt="">
+                    <img src="{{ asset('img/facebook_footer.svg') }}" alt="">
+                    <img src="{{ asset('img/tiktok_footer.svg') }}" alt="">
+                    <img src="{{ asset('img/telegram_footer.svg') }}" alt="">
+                    <img src="{{ asset('img/instagram_footer.svg') }}" alt="">
+                </div>
+                <p class="text-xs">співпраця - serafim.nation@gmail.com</p>
+            </div>
+        </div>
+        <div class="w-1/6 text-white flex flex-col gap-1">
+            <p class="text-yellow-400 text-2xl decoration-yellow-400 decoration-8 underline-offset-4 mb-3"><u>ca</u>йт</p>
+            <p>Що таке комплект знань</p>
+            <p>Обрати комплект знань</p>
+            <p>Юрист онлайн</p>
+            <p>Про автора</p>
+            <p>Соцмережі</p>
+            <p>Безкоштовно</p>
+        </div>
+        <div class="w-1/5 text-white flex flex-col gap-1">
+            <p class="text-yellow-400 text-2xl decoration-yellow-400 decoration-8 underline-offset-4 mb-3"><u>ко</u>мплекти</p>
+            <p>Комплект громадянина</p>
+            <p>Комплект військовослужбовця</p>
+            <p>Комплект військовослужбовця НГУ</p>
+            <p>Комплект поліцейського</p>
+            <p>Зібрати власний комплект</p>
+        </div>
+        <div class="w-1/5 text-white flex flex-col gap-1">
+            <p class="text-yellow-400 text-2xl decoration-yellow-400 decoration-8 underline-offset-4 mb-3"><u>ку</u>ди звертатись</p>
+            <p>Міністерство оборони України</p>
+            <p>Військова служба правопорядку</p>
+            <p>Державне бюро розслідувань</p>
+            <p>Національне антикорупційне бюро</p>
+            <p>Юридична сотня</p>
+            <p>Омбудсман</p>
+            <p>НАЗК</p>
+        </div>
+    </div>
+
+    <hr class="w-7/12 mx-auto mt-10">
+    <div class="text-white my-3 flex items-center justify-center gap-2">
+        terms of use <span class="text-4xl">|</span> privacy policy
+    </div>
+    <hr class="w-7/12 mx-auto">
 
 </footer>
 </body>
