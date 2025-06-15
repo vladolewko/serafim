2f0bbaf437095ee3b3eae1d34a847031e286b998
Evg33n <evgen.gulevaty@gmail.com>
2025-06-15 15:39:43 +0300

    fix select bug and simplify create order script code

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -490,7 +490,7 @@
                         <div class="bg-white flex flex-col items-center rounded-xl ">
                             <!-- posible img -->
 
-                            <div class="h-[212px] w-[212px] rounded-xl bg-gray-200 m-3 slef-center">
+                            <div class="h-[212px] w-[212px] rounded-xl bg-gray-200  m-3 slef-center overflow-hidden">
                                 @if($product->getMedia('product_images')->isNotEmpty())
                                     <img src="{{ $product->getFirstMediaUrl('product_images') }}" alt="{{ $product->title }}">
                                 @endif
