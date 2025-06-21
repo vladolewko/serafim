fc8f81b0bafd4b1216a0d33df0c7a763857fc754
Evg33n <evgen.gulevaty@gmail.com>
2025-06-21 21:28:50 +0300

    remake haeder and introduction section

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -6,60 +6,20 @@
 
 @section('content')
 
-            <div class="bg-blue-400 text-white w-full">
-                <div class="w-full px-4 sm:px-6 lg:w-4/6 lg:mx-auto">
-                    <div class="flex flex-col lg:flex-row items-center py-8 lg:py-16">
-                        <div class="w-full lg:w-3/6 mb-8 lg:mb-0">
-                            <p class="text-2xl sm:text-3xl lg:text-5xl font-bold leading-tight">
-                                Захищаєш Україну? Навчися <span class="text-yellow-400">захищати себе!</span>
-                            </p>
-                            <p class="text-lg sm:text-xl lg:text-2xl text-slate-200 my-3 lg:my-6">
-                                Комплекти перевірених юридичних знань для військових, НГУ, поліцейських і громадян.
-                            </p>
-                            <div class="flex flex-col sm:flex-row gap-4 sm:gap-0">
-                                <button
-                                    class="hidden lg:block bg-yellow-400 px-6 py-3 rounded-lg text-black text-lg lg:text-xl font-semibold knowledgePackBtn" data-target="knowledge_pack">
-                                    комплект знань
-                                </button>
-                                <!-- mobile button -->
-                                <button
-                                    class="lg:hidden bg-yellow-400 px-6 py-3 rounded-lg text-black text-lg lg:text-xl font-semibold knowledgePackBtn" data-target="knowledge_pack_mobile">
-                                    комплект знань
-                                </button>
-                                <p class="text-black text-lg lg:text-xl underline underline-offset-4 decoration-yellow-400 self-center sm:mx-4">
-                                    короткий ролик
-                                </p>
-                            </div>
-                        </div>
-                        <div class="w-full lg:w-3/6">
-                            <div class="xl:justify-self-end">
-                    <span class="text-xl lg:text-2xl text-yellow-400 mt-4 lg:mt-12 inline-block font-semibold">
-                        Тематика школи
-                    </span>
-                                <ul class="list-disc ml-5 lg:ml-8 text-sm lg:text-base leading-relaxed w-11/12 sm:w-full lg:w-11/12 mt-2">
-                                    <li>Взаємодія з державними органами влади та засобами масової інформації (ЗМІ)</li>
-                                    <li>Правовий захист військових і цивільних (СЗЧ, пільги, рапорти, переведення)</li>
-                                    <li>Психологічна підтримка мобілізованих, родин, повернення з війни</li>
-                                    <li>Освітні інструменти для родин зниклих безвісти, військовополонених</li>
-                                    <li>Антикорупційна боротьба, права викривачів, звернення до НАЗК/НАБУ</li>
-                                    <li>Соціальні курси (дискримінація, сексуальні домагання в армії)</li>
-                                    <li>Практичні гайди: шаблони, закони, інструкції, чек-листи</li>
-                                    <li>Цільова аудиторія: військові, їхні родини, мобілізовані, громадяни</li>
-                                </ul>
-                                <span
-                                    class="text-xl lg:text-2xl text-yellow-400 mt-4 lg:mt-6 inline-block font-semibold">
-                        Мій підхід
-                    </span>
-                                <ul class="list-disc ml-5 lg:ml-8 mb-8 lg:mb-12 text-sm lg:text-base leading-relaxed w-11/12 sm:w-full lg:w-11/12 mt-2">
-                                    <li>Практичність: реальні інструкці, шаблони, приклади звернень</li>
-                                    <li>Соціальна місія: підтримка, допомога й відновлення справедливості</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
+
+            <!-- Introduction and video -->
+            <!-- @if(isset($_POST['screenWidth']) && $_POST['screenWidth'] > 1025)
+                @include('components.introduction')
+            @else
+                @include('components.introduction-mobile')
+            @endif -->
+
+
+            <div class="hidden sm:block"> @include('components.introduction')</div>
+            <div class="sm:hidden"> @include('components.introduction-mobile') </div>
+
+            </div>
             <!-- Why Important Section -->
             <div
                 class="flex flex-col lg:flex-row my-16 lg:my-52 w-full px-4 sm:px-6 xl:w-4/6 lg:w-5/6 lg:mx-auto gap-8 lg:justify-between">
