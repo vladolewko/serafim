e1d10f95876b1a9623d5aee2fba2396a809f7bae
vlad <vlad.olewko@gmai.com>
2025-06-25 19:57:39 +0300

    favicon.svg

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                "flowbite/dist/flowbite.css",
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/js/index.js",
