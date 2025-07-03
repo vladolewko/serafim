aa75734a2417074272a91ebde8228eb7bffa2723
Evg33n <evgen.gulevaty@gmail.com>
2025-07-03 17:28:40 +0300

    fix header and replace logo, add icons to introduction buttons

--- resources/views/components/goverment_agencies.blade.php
+++ resources/views/components/goverment_agencies.blade.php
@@ -22,15 +22,25 @@
         </div>
 
         <div class="flex flex-col items-center">
-            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/national_police.png') }}" alt="">
-             <p class="text-blue-400 text-center">Національна поліція</p>
+            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/DBR_logo.png') }}" alt="">
+             <p class="text-blue-400 text-center">ДБР</p>
         </div>
 
         <div class="flex flex-col items-center">
-            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/NAZK_logo.png') }}" alt="">
-             <p class="text-blue-400 text-center">НАЗК</p>
+            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/SBU.png') }}" alt="">
+             <p class="text-blue-400 text-center">СБУ</p>
         </div>
+
+        <div class="flex flex-col items-center">
+            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/SUDY.png') }}" alt="">
+            <p class="text-blue-400 text-center">СУДИ</p>
+        </div>
+
+
                 </div>
+
+
+
             </div>
             <!-- Item 2 -->
 
@@ -41,47 +51,36 @@
              <p class="text-blue-400 text-center">МВС</p>
         </div>
 
-        <div class="flex flex-col items-center">
-            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/SBU.png') }}" alt="">
-             <p class="text-blue-400 text-center">СБУ</p>
+         <div class="flex flex-col items-center">
+            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/national_police.png') }}" alt="">
+             <p class="text-blue-400 text-center">Національна поліція</p>
         </div>
 
         <div class="flex flex-col items-center">
-            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/DBR_logo.png') }}" alt="">
-             <p class="text-blue-400 text-center">ДБР</p>
+            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/NAZK_logo.png') }}" alt="">
+             <p class="text-blue-400 text-center">НАЗК</p>
         </div>
 
         <div class="flex flex-col items-center">
             <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/commissioner.png') }}" alt="">
              <p class="text-blue-400 text-center">Омбудсмен</p>
         </div>
-                </div>
-            </div>
-            <!-- Item 3 -->
 
-            <div class="hidden duration-700 ease-in-out bg-white" data-carousel-item>
-                <div class="flex gap-10  justify-center">
-                    <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center">
             <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/ministry.png') }}" alt="">
              <p class="text-blue-400 text-center">Міністерство оборони</p>
         </div>
 
-        <div class="flex flex-col items-center">
-            <img class="w-[150px] h-[150px] border border-blue-400 rounded-full" src="{{ asset('img/SUDY.png') }}" alt="">
-            <p class="text-blue-400 text-center">СУДИ</p>
-        </div>
                 </div>
             </div>
         </div>
         <!-- Slider indicators -->
         <div
             class="absolute z-30 flex -translate-x-1/2 space-x-0 rtl:space-x-reverse left-1/2 bottom-[-20px] w-full justify-center">
-            <button type="button" class="w-[30%] h-[1px] rounded-full  slide_button" aria-current="true"
+            <button type="button" class="w-[50%] h-[1px] rounded-full  slide_button" aria-current="true"
                     aria-label="Slide 1" data-carousel-slide-to="0"></button>
-            <button type="button" class="w-[30%] h-[1px] rounded-full  slide_button" aria-current="false"
+            <button type="button" class="w-[50%] h-[1px] rounded-full  slide_button" aria-current="false"
                     aria-label="Slide 2" data-carousel-slide-to="1"></button>
-            <button type="button" class="w-[30%] h-[1px] rounded-full  slide_button" aria-current="false"
-                    aria-label="Slide 3" data-carousel-slide-to="2"></button>
         </div>
         <!-- Slider controls -->
 
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,12 +6,11 @@
         @include('components.navigation')
         <div class="text-white flex justify-center items-center gap-3 flex-wrap sm:flex-nowrap">
             <div class="text-center">
-                <p class="text-sm">
-                    Авторська онлайн-школа
-                </p class="text-sm">
-                <p>правового виховання</p>
-                <p class="text-2xl">
-                    “Серафим”
+                <p class="text-base font-bold">
+                    Авторські курси правового виховання
+                </p>
+                <p class="text-2xl font-bold text-yellow-400">
+                    СЕРАФИМ
                 </p>
                 <!-- <p class="text-sm">
                     Від солдата - для людей
@@ -20,7 +19,7 @@
 
             <img src="{{ asset('img/line_vertical.svg') }}" alt="">
 
-            <img class="sm:h-[81px] sm:w-[81px] h-[75px] w-[75px]" src="{{ asset('img/logo.svg') }}" alt="logo">
+            <img class="bg-white rounded-full sm:h-[81px] sm:w-[81px] h-[75px] w-[75px]" src="{{ asset('img/logo_1.png') }}" alt="logo">
 
             <img class="h-0 w-0 sm:h-[81px] w-[1px]" src="{{ asset('img/line_vertical.svg') }}" alt="">
 
@@ -44,117 +43,7 @@
         <nav class="bg-blue-400">
         <div class="max-w-screen-xl gap-1 flex flex-wrap items-center justify-between mx-auto p-2" >
 
-            <!-- <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg lg:hidden focus:outline-none dark:text-white dark:hover:bg-gray-700 " aria-controls="navbar-default" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button> -->
-
-
-
-            <!-- <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-white rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-               <span class="sr-only">Open sidebar</span>
-               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button> -->
-
-            <!-- <aside id="default-sidebar" class="fixed top-0 left-0 z-50 w-64 h-screen transition-transform -translate-x-full lg:translate-x-0" aria-label="Sidebar">
-                <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="text-white bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
-                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    <span class="sr-only">Close menu</span>
-                </button>
-               <div class="h-full px-3 py-4 overflow-y-auto bg-blue-500">
-                  <ul class="space-y-2 font-medium">
-                     <!-- <li>
-                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                           <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                              <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                              <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-                           </svg>
-                           <span class="ms-3">Dashboard</span>
-                        </a>
-                     </li>
-                     <li>
-                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                           <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                              <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
-                           </svg>
-                           <span class="flex-1 ms-3 whitespace-nowrap">Kanban</span>
-                           <span class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
-                        </a>
-                     </li>
-                     <li>
-                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                           <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                              <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-                           </svg>
-                           <span class="flex-1 ms-3 whitespace-nowrap">Inbox</span>
-                           <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
-                        </a>
-                     </li>
-                     <li>
-                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                           <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                              <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-                           </svg>
-                           <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
-                        </a>
-                     </li>
-                     <li>
-                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                           <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                              <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
-                           </svg>
-                           <span class="flex-1 ms-3 whitespace-nowrap">Products</span>
-                        </a>
-                     </li>
-                     <li>
-                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                           <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-                           </svg>
-                           <span class="flex-1 ms-3 whitespace-nowrap">Sign In</span>
-                        </a>
-                     </li>
-                     <li>
-                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                           <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                              <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                              <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                              <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-                           </svg>
-                           <span class="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
-                        </a>
-                     </li>
-                     <li>
-
-                     </li>
-                     <li class="m-0">
-                        <a class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 md:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent nav-item" data-target="wt_knowledge_pack">що таке комплект знань</a>
-                    </li>
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 md:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent nav-item" data-target="knowledge_pack">обрати комплект</a>
-                    </li>
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 md:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent nav-item" data-target="about_author">про автора</a>
-                    </li>
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 md:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent nav-item" data-target="social">соцмережі</a>
-                    </li>
-                    <li>
-                        <a class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 md:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent nav-item" data-target="for_free">безкоштовно</a>
-                    </li>
-                  </ul>
-               </div>
-            </aside> -->
-
-
-
-
-
-            <button class="inline-flex items-center p-2 mt-2 md:mt-0 ms-3 text-sm text-white rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2  dark:hover:bg-gray-700" type="button" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
+            <button class="inline-flex items-center p-2  md:mt-0 ms-3 text-sm text-white rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2  dark:hover:bg-gray-700" type="button" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
             <span class="sr-only">Open sidebar</span>
                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -207,21 +96,22 @@
             </div>
 
 
-            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <div class="text-right text-white">
-                <p class="text-xs">
-                    Авторська онлайн-школа
-                </p>
-                <p  class="text-xs">правового виховання</p>
-                <p class="text-xl">
-                    “Серафим”
-                </p>
-                <!-- <p class="text-xs">
-                    Від солдата - для людей
-                </p> -->
+            <div class="text-right text-white">
+                    <p class="text-[10px] font-extrabold">
+                        Авторські курси правового виховання
+                    </p>
+
+                    <p class="text-xl text-center font-bold text-yellow-400">
+                        СЕРАФИМ
+                    </p>
             </div>
-                <img class="h-[65px] w-[65px]" src="{{ asset('img/mobile-logo.svg') }}" alt="logo">
-            </a>
+
+
+            <img class="rounded-full h-[65px] w-[65px] object-cover m-0 p-0" style="background-color: white" src="{{ asset('img/logo_1.png') }}" alt="logo">
+
+
+
+
             <!-- <div class="text-white w-11/12 mx-auto lg:hidden md:w-2/5">
                 <p class="text-left text-base font-bold flex-1">
                     Стаття 68 Конституції України:
@@ -253,10 +143,10 @@
         </div>
         <hr class="bg-white border-0 h-px mx-auto w-full">
          <div class="text-white w-11/12 mx-auto lg:hidden py-2  md:w-3/5">
-                <p class="text-left text-base font-bold flex-1">
+                <p class="text-left text-sm font-bold flex-1">
                     Стаття 68 Конституції України:
                 </p>
-                <p class="text-sm/[1]">
+                <p class="text-[10px]/[1]">
                     Кожен зобов'язаний неухильно додержуватися Конституції
                     України та законів України, не посягати на права і свободи, честь і гідність інших людей.
                 </p>
