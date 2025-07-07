b18559733fe205a9f422847973a6d58a40250ce0
Evg33n <evgen.gulevaty@gmail.com>
2025-07-07 20:10:25 +0300

    change icons at how to submit component, change carousel step in goverment component and add disable card in choose knoweledge carousel

--- resources/views/components/chooseknoweledge-mobile.blade.php
+++ resources/views/components/chooseknoweledge-mobile.blade.php
@@ -35,7 +35,78 @@
                         </div>
                     </div>
                 </div>
+
             @endforeach
+
+
+
+
+                <div class="hidden duration-500 ease-in-out bg-white" data-carousel-item>
+                    <div class="flex justify-center mt-10">
+                        <div class="w-8/12 md:w-4/12 lg:w-3/12 xl:w-1/5 h-full rounded-xl bg-gray-200 p-[2px]">
+                            <div class="bg-white flex flex-col items-center rounded-xl">
+                                <!-- posible img -->
+                                <div class="h-[212px] w-[212px] rounded-xl bg-gray-200 m-3 slef-center overflow-hidden">
+                                    <!-- @if($product->getMedia('product_images')->isNotEmpty())
+                                        <img class="w-full h-full" src="{{ $product->getFirstMediaUrl('product_images') }}"
+                                                alt="{{ $product->title }}">
+                                    @endif -->
+                                </div>
+
+                                <p class="text-xl/6 font-semibold text-center w-4/6">В РОЗРОБЦІ</p>
+                                <p class="text-3xl font-bold text-center my-4" style="font-weight: 700">0 грн</p>
+                                <a class="flex items-center justify-center bg-gray-400 w-11/12 text-center m-2 rounded-lg inline-block align-middle h-10 text-black font-bold text-xl shadow-md cursor-default"
+                                    href="{{ route('product.show', $product->id) }}">переглянути</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="hidden duration-500 ease-in-out bg-white" data-carousel-item>
+                    <div class="flex justify-center mt-10">
+                        <div class="w-8/12 md:w-4/12 lg:w-3/12 xl:w-1/5 h-full rounded-xl bg-gray-200 p-[2px]">
+                            <div class="bg-white flex flex-col items-center rounded-xl">
+                                <!-- posible img -->
+                                <div class="h-[212px] w-[212px] rounded-xl bg-gray-200 m-3 slef-center overflow-hidden">
+                                    <!-- @if($product->getMedia('product_images')->isNotEmpty())
+                                        <img class="w-full h-full" src="{{ $product->getFirstMediaUrl('product_images') }}"
+                                                alt="{{ $product->title }}">
+                                    @endif -->
+                                </div>
+
+                                <p class="text-xl/6 font-semibold text-center w-4/6">В РОЗРОБЦІ</p>
+                                <p class="text-3xl font-bold text-center my-4" style="font-weight: 700">0 грн</p>
+                                <a class="flex items-center justify-center bg-gray-400 w-11/12 text-center m-2 rounded-lg inline-block align-middle h-10 text-black font-bold text-xl shadow-md cursor-default"
+                                    href="{{ route('product.show', $product->id) }}">переглянути</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="hidden duration-500 ease-in-out bg-white" data-carousel-item>
+                    <div class="flex justify-center mt-10">
+                        <div class="w-8/12 md:w-4/12 lg:w-3/12 xl:w-1/5 h-full rounded-xl bg-gray-200 p-[2px]">
+                            <div class="bg-white flex flex-col items-center rounded-xl">
+                                <!-- posible img -->
+                                <div class="h-[212px] w-[212px] rounded-xl bg-gray-200 m-3 slef-center overflow-hidden">
+                                    <!-- @if($product->getMedia('product_images')->isNotEmpty())
+                                        <img class="w-full h-full" src="{{ $product->getFirstMediaUrl('product_images') }}"
+                                                alt="{{ $product->title }}">
+                                    @endif -->
+                                </div>
+
+                                <p class="text-xl/6 font-semibold text-center w-4/6">В РОЗРОБЦІ</p>
+                                <p class="text-3xl font-bold text-center my-4" style="font-weight: 700">0 грн</p>
+                                <a class="flex items-center justify-center bg-gray-400 w-11/12 text-center m-2 rounded-lg inline-block align-middle h-10 text-black font-bold text-xl shadow-md cursor-default"
+                                    href="{{ route('product.show', $product->id) }}">переглянути</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
         </div>
 
         <!-- Slider indicators -->
--- resources/views/components/goverment_agencies.blade.php
+++ resources/views/components/goverment_agencies.blade.php
@@ -1,15 +1,15 @@
 <div class="relative w-[76%] mx-auto">
-    <div class="bg-blue-500 py-2 px-4 text-xl text-white absolute top-[-20px] left-0">
+    <!-- <div class="bg-blue-500 py-2 px-4 text-xl text-white absolute top-[-20px] left-0">
         ВЧИМОСЯ ВЗАЄМОДІЯТИ З ОРГАНАМИ ВЛАДИ
-    </div>
+    </div> -->
 
-      <div id="indicators-carousel" class="relative pt-10 w-full" data-carousel="static">
+      <!-- <div id="indicators-carousel" class="relative pt-10 w-full" data-carousel="static"> -->
         <!-- Carousel wrapper -->
 
-        <div class="relative h-56 overflow-hidden rounded-lg md:h-[200px] my-8">
+        <!-- <div class="relative h-56 overflow-hidden rounded-lg md:h-[200px] my-8"> -->
             <!-- Item 1 -->
 
-            <div class="hidden duration-700 ease-in-out bg-white" data-carousel-item>
+            <!-- <div class="hidden duration-700 ease-in-out bg-white" data-carousel-item>
                 <div class="flex gap-10  justify-center">
                     <div class="flex flex-col items-center">
                 <img class="w-[150px] h-[150px]" src="{{ asset('img/general_procoratura.png') }}" alt="procoratura img">
@@ -41,10 +41,10 @@
 
 
 
-            </div>
+            </div> -->
             <!-- Item 2 -->
 
-        <div class="hidden duration-700 ease-in-out bg-white" data-carousel-item>
+        <!-- <div class="hidden duration-700 ease-in-out bg-white" data-carousel-item>
                 <div class="flex gap-10  justify-center">
                     <div class="flex flex-col items-center">
             <img class="w-[150px] h-[150px] " src="{{ asset('img/MVS.png') }}" alt="">
@@ -53,7 +53,7 @@
 
          <div class="flex flex-col items-center">
             <img class="w-[150px] h-[150px] " src="{{ asset('img/national_police.png') }}" alt="">
-             <p class="text-blue-400 text-center">Національна поліція</p>
+            <p class="text-blue-400 text-center">Національна поліція</p>
         </div>
 
         <div class="flex flex-col items-center">
@@ -83,7 +83,7 @@
                     </div>
 
                     <div class="flex flex-col items-center">
-                        <img class="w-[150px] h-[150px]  rounded-full" src="{{ asset('img/VSP.png') }}" alt="vsp img">
+                        <img class="w-[150px] h-[150px]  rounded-full" src="{{ asset('img/VSP.png') }}" aria-label="Slide 1"alt="vsp img">
                         <p class="text-blue-400 text-center">ВСП</p>
                     </div>
 
@@ -94,9 +94,9 @@
 
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- Slider indicators -->
-        <div
+        <!-- <div
             class="absolute z-30 flex -translate-x-1/2 space-x-0 rtl:space-x-reverse left-1/2 bottom-[-20px] w-full justify-center">
             <button type="button" class="w-[33%] h-[1px] rounded-full  slide_button" aria-current="true"
                     aria-label="Slide 1" data-carousel-slide-to="0"></button>
@@ -104,10 +104,10 @@
                     aria-label="Slide 2" data-carousel-slide-to="1"></button>
             <button type="button" class="w-[33%] h-[1px] rounded-full  slide_button" aria-current="false"
                     aria-label="Slide 3" data-carousel-slide-to="1"></button>
-        </div>
+        </div> -->
         <!-- Slider controls -->
 
-        <button type="button"
+        <!-- <button type="button"
                 class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
                 data-carousel-prev>
         <span class="inline-flex items-center justify-center">
@@ -125,9 +125,233 @@
             <span class="sr-only">Next</span>
         </span>
         </button>
+    </div> -->
+
+
+
+
+
+        <div class="bg-blue-500 py-2 px-4 text-xl text-white absolute top-[-20px] left-0">
+        ВЧИМОСЯ ВЗАЄМОДІЯТИ З ОРГАНАМИ ВЛАДИ
+        </div>
+
+
+        <div class="relative pt-10 w-full">
+            <!-- Carousel wrapper -->
+            <div class="relative h-56 overflow-hidden rounded-lg md:h-[200px] my-8">
+                <div class="flex transition-transform duration-700 ease-in-out" id="carousel-container">
+                    <!-- Всі елементи в одному контейнері -->
+
+                    <!-- Item 1 - Генеральна прокаратура -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px]" src="{{ asset('img/general_procoratura.png') }}" alt="procoratura img">
+                            <p class="text-blue-400 text-center">Генеральна прокаратура</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 2 - НАБУ -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                           <img class="w-[150px] h-[150px] " src="{{ asset('img/nabu.svg') }}" alt="">
+                            <p class="text-blue-400 text-center">НАБУ</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 3 - ДБР -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px] rounded-full" src="{{ asset('img/DBR_logo.png') }}" alt="">
+                            <p class="text-blue-400 text-center">ДБР</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 4 - СБУ -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px] " src="{{ asset('img/SBU.png') }}" alt="">
+                            <p class="text-blue-400 text-center">СБУ</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 5 - СУДИ -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px] " src="{{ asset('img/SUDY.png') }}" alt="">
+                            <p class="text-blue-400 text-center">СУДИ</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 6 - МВС -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px] " src="{{ asset('img/MVS.png') }}" alt="">
+                            <p class="text-blue-400 text-center">МВС</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 7 - Національна поліція -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px] " src="{{ asset('img/national_police.png') }}" alt="">
+                            <p class="text-blue-400 text-center">Національна поліція</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 8 - НАЗК -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px] rounded-full" src="{{ asset('img/NAZK_logo.png') }}" alt="">
+                            <p class="text-blue-400 text-center">НАЗК</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 9 - Омбудсмен -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px] " src="{{ asset('img/commissioner.png') }}" alt="">
+                            <p class="text-blue-400 text-center">Омбудсмен</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 10 - Міністерство оборони -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px] " src="{{ asset('img/ministry.png') }}" alt="">
+                            <p class="text-blue-400 text-center">Міністерство оборони</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 11 - Командування -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px] rounded-full" src="{{ asset('img/command.png') }}" alt="command img">
+                            <p class="text-blue-400 text-center">Командування</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 12 - ВСП -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px]  rounded-full" src="{{ asset('img/VSP.png') }}" alt="vsp img">
+                            <p class="text-blue-400 text-center">ВСП</p>
+                        </div>
+                    </div>
+
+                    <!-- Item 13 - Міністерство ветеранів -->
+                    <div class="flex-shrink-0 w-1/5 px-2">
+                        <div class="flex flex-col items-center">
+                            <img class="w-[150px] h-[150px]  rounded-full" src="{{ asset('img/ministry_of_veterans.png') }}" alt="ministry_veterans img">
+                            <p class="text-blue-400 text-center">Міністерство ветеранів</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+           <div class="absolute z-30 flex -translate-x-1/2 space-x-0 rtl:space-x-reverse left-1/2 bottom-[-30px] w-full justify-center">
+                <button type="button" class="w-[10%] h-[1px] rounded-full bg-yellow-400 hover:bg-yellow-500 transition-colors indicator active" data-slide="0"></button>
+                <button type="button" class="w-[10%] h-[1px] rounded-full bg-blue-400 hover:bg-gray-500 transition-colors indicator" data-slide="1"></button>
+                <button type="button" class="w-[10%] h-[1px] rounded-full bg-blue-400 hover:bg-gray-500 transition-colors indicator" data-slide="2"></button>
+                <button type="button" class="w-[10%] h-[1px] rounded-full bg-blue-400 hover:bg-gray-500 transition-colors indicator" data-slide="3"></button>
+                <button type="button" class="w-[10%] h-[1px] rounded-full bg-blue-400 hover:bg-gray-500 transition-colors indicator" data-slide="4"></button>
+                <button type="button" class="w-[10%] h-[1px] rounded-full bg-blue-400 hover:bg-gray-500 transition-colors indicator" data-slide="5"></button>
+                <button type="button" class="w-[10%] h-[1px] rounded-full bg-blue-400 hover:bg-gray-500 transition-colors indicator" data-slide="6"></button>
+                <button type="button" class="w-[10%] h-[1px] rounded-full bg-blue-400 hover:bg-gray-500 transition-colors indicator" data-slide="7"></button>
+                <button type="button" class="w-[10%] h-[1px] rounded-full bg-blue-400 hover:bg-gray-500 transition-colors indicator" data-slide="8"></button>
+            </div>
+
+
+            <!-- Slider controls -->
+            <button type="button" class="absolute top-0 start-[-30px] z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" id="prev-btn">
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full group-focus:outline-none">
+                    <img class="w-8 h-8 text-white dark:text-gray-800 rtl:rotate-180"
+                    src="{{ asset('img/button_carousel_left.svg') }}" alt="button_carousel_left">
+                    <span class="sr-only">Previous</span>
+                </span>
+            </button>
+
+            <button type="button" class="absolute top-0 end-[-30px] z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" id="next-btn">
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full group-focus:outline-none">
+                    <img class="w-8 h-8 text-white dark:text-gray-800 rtl:rotate-180"
+                    src="{{ asset('img/button_carousel_right.svg') }}" alt="button_carousel_right">
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>
+        </div>
     </div>
 
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const container = document.getElementById('carousel-container');
+            const prevBtn = document.getElementById('prev-btn');
+            const nextBtn = document.getElementById('next-btn');
+            const indicators = document.querySelectorAll('.indicator');
 
+            let currentSlide = 0;
+            const totalItems = 13;
+            const itemsVisible = 5;
+            const maxSlide = totalItems - itemsVisible; // 8 позицій (0-8)
+
+            function updateCarousel() {
+                // Рухаємо контейнер на 20% (100% / 5 елементів = 20%)
+                const translateX = currentSlide * 20;
+                container.style.transform = `translateX(-${translateX}%)`;
+
+                // Оновлюємо індикатори
+                indicators.forEach((indicator, index) => {
+                    if (index === currentSlide) {
+                        indicator.classList.remove('bg-blue-400');
+                        indicator.classList.add('bg-yellow-400', 'active');
+                    } else {
+                        indicator.classList.remove('bg-yellow-400', 'active');
+                        indicator.classList.add('bg-blue-400');
+                    }
+                });
+            }
+
+            function nextSlide() {
+                if (currentSlide < maxSlide) {
+                    currentSlide++;
+                    updateCarousel();
+                }
+            }
+
+            function prevSlide() {
+                if (currentSlide > 0) {
+                    currentSlide--;
+                    updateCarousel();
+                }
+            }
+
+            // Обробники подій
+            nextBtn.addEventListener('click', nextSlide);
+            prevBtn.addEventListener('click', prevSlide);
+
+            // Обробники для індикаторів
+            indicators.forEach((indicator, index) => {
+                indicator.addEventListener('click', () => {
+                    currentSlide = index;
+                    updateCarousel();
+                });
+            });
+
+            // Ініціалізація
+            updateCarousel();
+
+            // Автоматичне прокручування (опціонально)
+            /*
+            setInterval(() => {
+                if (currentSlide < maxSlide) {
+                    nextSlide();
+                } else {
+                    currentSlide = 0;
+                    updateCarousel();
+                }
+            }, 3000);
+            */
+        });
+    </script>
 
 
 
