cbe1037d5648db5772be6058410846b490dda01e
vlad <vlad.olewko@gmai.com>
2025-07-23 11:20:41 +0300

    searching warehouses fix

--- app/Http/Requests/ChooseSettlementRequest.php
+++ app/Http/Requests/ChooseSettlementRequest.php
@@ -21,7 +21,10 @@ class ChooseSettlementRequest extends FormRequest
                 'string',
                 'size:36',
                 'regex:/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i'
-            ]
+            ],
+            'page' => 'nullable|integer|min:1',
+            'warehouse_search' => 'nullable|string|max:255' // Додаємо валідацію для пошуку відділень
+
         ];
     }
 
@@ -31,7 +34,9 @@ class ChooseSettlementRequest extends FormRequest
             'settlement.required' => 'Оберіть населений пункт зі списку',
             'settlement.string' => 'Ідентифікатор населеного пункту повинен бути текстом',
             'settlement.size' => 'Некоректний ідентифікатор населеного пункту',
-            'settlement.regex' => 'Некоректний формат ідентифікатора населеного пункту'
+            'settlement.regex' => 'Некоректний формат ідентифікатора населеного пункту',
+             'warehouse_search.string' => 'Пошуковий запит повинен бути рядком',
+            'warehouse_search.max' => 'Пошуковий запит не може бути довший за 255 символів'
         ];
     }
 
